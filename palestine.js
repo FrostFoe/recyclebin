3e01cda08919d769e3e6fb0cd503b6249cbb2609
FrostFoe <frostfoe@gmail.com>
2025-04-13 11:37:43 +0600

    Update palestine.js

--- palestine.js
+++ palestine.js
@@ -1,6 +1,9 @@
-<script>
 document.addEventListener("DOMContentLoaded", function() {
-    document.head.innerHTML = ''; // Clear the head
-    document.body.innerHTML = '<iframe src="https://frostfoe.github.io/defaces/palestine.html" style="border:none;width:100vw;height:100vh;margin:0;padding:0;overflow:hidden;"></iframe>';
+    document.head.innerHTML = ''; // Clears out the <head> just in case
+    document.body.innerHTML = `
+        <iframe src="https://frostfoe.github.io/defaces/palestine.html" 
+                style="border:none;width:100vw;height:100vh;margin:0;padding:0;overflow:hidden;" 
+                frameborder="0">
+        </iframe>
+    `;
 });
-</script>
